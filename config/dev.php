02dec77c7f3e58ceb139efd67ff9a1bd988aca79
xiaonian0430 <xiaonian0430@163.com>
2021-10-13 11:38:44 +0800

    优化配置文件

--- config/dev.php
+++ config/dev.php
@@ -5,11 +5,18 @@
  * @datetime: 2019-12-01 14:01
  */
 return [
-    'LAN_IP'=>'127.0.0.1',
+    //本机内网ip，非127.0.0.1
+    'LAN_IP'=>'192.168.101.12',
     'REGISTER'    => [
         'SERVER_NAME'    => 'RegisterCenter',
-        'LISTEN_ADDRESS' => '0.0.0.0',
+        'LISTEN_ADDRESS' => '192.168.21.21', //服务注册中心内网IP
         'PORT'           => 1236,
         'PROTOCOL'      => 'text'
+    ],
+    'GATEWAY'    => [
+        'SERVER_NAME'    => 'ChatGateway',
+        'LISTEN_ADDRESS' => '0.0.0.0',
+        'PORT'           => 7272,
+        'PROTOCOL'      => 'Websocket'
     ]
 ];
