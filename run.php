04e89cb93a241495f106652ccfef3bc28e52f29b
xiaonian0430 <xiaonian0430@163.com>
2021-11-02 15:05:48 +0800

    优化启动文件

--- run.php
+++ run.php
@@ -5,31 +5,13 @@
  * @contact: xiaonian030@163.com
  * @datetime: 2021-09-14 10:00
  */
-use Workerman\Worker;
-use GatewayWorker\Register;
+
+use HP\Register\App;
 
 //初始化
 ini_set('display_errors', 'on');
 defined('IN_PHAR') or define('IN_PHAR', boolval(\Phar::running(false)));
 defined('SERVER_ROOT') or define('SERVER_ROOT', IN_PHAR ? \Phar::running() : realpath(getcwd()));
-
-//创建临时目录
-$temp_path=SERVER_ROOT.'/temp';
-$log_path=SERVER_ROOT.'/temp/log';
-if(!is_dir($log_path)){
-    mkdir($log_path, 0777, true);
-}
-
-// 检查扩展或环境
-if(strpos(strtolower(PHP_OS), 'win') === 0) {
-    exit("start.php not support windows.\n");
-}
-if(!extension_loaded('pcntl')) {
-    exit("Please install pcntl extension.\n");
-}
-if(!extension_loaded('posix')) {
-    exit("Please install posix extension.\n");
-}
 
 //自动加载文件
 $auto_file=SERVER_ROOT . '/vendor/autoload.php';
@@ -39,32 +21,8 @@ if (file_exists($auto_file)) {
     exit("Please composer install.\n");
 }
 
-//导入配置文件
-$mode='produce';
-foreach ($argv as $item){
-    $item_val=explode('=', $item);
-    if(count($item_val)==2 && $item_val[0]=='-mode'){
-        $mode=$item_val[1];
-    }
-}
-$config_path=SERVER_ROOT . '/config/'.$mode.'.php';
-if (file_exists($config_path)) {
-    $conf = require_once $config_path;
-}else{
-    exit($config_path." is not exist\n");
-}
-defined('CONFIG') or define('CONFIG', $conf);
+//初始化应用
+$app = new App();
 
-//初始化worker
-Worker::$stdoutFile = $log_path.'/error.log';
-Worker::$logFile = $log_path.'/log.log';
-Worker::$pidFile = $temp_path.'/pid.pid';
-
-$address='text://'.CONFIG['REGISTER']['LISTEN_ADDRESS'].':'.CONFIG['REGISTER']['PORT'];
-
-// register 服务必须是text协议
-$register = new Register($address);
-$register->name=CONFIG['REGISTER']['SERVER_NAME'];
-
-// 运行所有服务
-Worker::runAll();
+//启动
+$app->run();
