4285a647fa068e649787007ad3b24358c00f6318
xiaonian0430 <xiaonian0430@163.com>
2021-11-02 16:54:51 +0800

    添加协程

--- config/test.php
+++ config/test.php
@@ -4,7 +4,10 @@
  * @contact: xiaonian030@163.com
  * @datetime: 2019-12-01 14:01
  */
+
+//SWOOLE 1 其他 0
 return [
+    'EVENT_LOOP'=>1,
     'REGISTER'    => [
         'SERVER_NAME'    => 'RegisterCenter',
         'LISTEN_ADDRESS' => '0.0.0.0',
